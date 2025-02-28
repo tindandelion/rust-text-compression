3a0412e5aeeb4d855247fb4c1fbe75c7f704848c
Sergey Moshnikov <s.moshnikov@gmail.com>
2025-02-28 07:14:24 +0100

    Updated performance-test binary

--- src/bin/performance-test.rs
+++ src/bin/performance-test.rs
@@ -2,16 +2,17 @@ use std::fs;
 use std::time::Instant;
 use text_compression::decode;
 use text_compression::encode_with_policy;
-use text_compression::policies::CaptureAll;
+use text_compression::policies::LimitLedgerSize;
 
-const INPUT_FILENAME: &str = "test-data/wap-6400.txt";
+const INPUT_FILENAME: &str = "test-data/wap-25600.txt";
 
 fn main() {
     println!("* Compressing {}...", INPUT_FILENAME);
     let source = fs::read_to_string(INPUT_FILENAME).unwrap();
+    let policy = LimitLedgerSize::with_max_size(32_768);
 
     let start = Instant::now();
-    let (encoded, substrings, _) = encode_with_policy(&source, CaptureAll);
+    let (encoded, substrings, _) = encode_with_policy(&source, policy);
     let decoded = decode(&encoded, &substrings);
     assert_eq!(decoded, source);
 
